cb1072a460a9b086acc1296c77c3584093dfce73
fabiofesistemas <fabiofvt@gmail.com>
2024-12-10 11:29:39 -0300

    Criação da API NestJS

--- nestjs-api/src/app.service.ts
+++ nestjs-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API Nest JS Rodando na Porta 3000';
   }
 }
